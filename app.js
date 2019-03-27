5c71b17a866faec0d69f57c87cad37d42924e57e
Dragos <dragos.bilaniuc@gmail.com>
2019-03-27 09:08:53 +0000

    connect to cloud database

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v9");
+mongoose.connect("mongodb+srv://dragos:dragos@yelpcamp-cla4h.mongodb.net/test?retryWrites=true");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
